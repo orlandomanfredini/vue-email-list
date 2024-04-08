0e1143cc010e5ef0357976a6b4bf977e19e5b1b9
orlando manfredini <orlando.manfre1@gmail.com>
2024-04-08 15:30:23 +0200

    ADD: sto cercando di generare sempre 10 mail senza concatenarle alle precedenti

--- js/app.js
+++ js/app.js
@@ -1,9 +1,51 @@
+// Utilizzando Vue
+// Attraverso l’apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare
+// 10 indirizzi email e stamparli in pagina all’interno di una lista.
+
+
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+
+
 const { createApp } = Vue
 
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        title: 'Api Email',
+        email: {},
+        emails: [],
       }
+    },
+    methods: {
+        generateEmail(){
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response)=>{
+                console.log(response.data);
+                this.email.text = response.data.response;
+            })
+        },
+
+        generateEmails(n){
+            array = [];
+            for(let i = 0; i < n; i++){
+                
+                axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((res)=>{
+                    
+                    array.push(res.data.response);
+                })
+                for(let j = 0; j < array.length; i++){
+                    this.emails.push(array[j]);
+                 }
+             }
+
+             
+           
+            
+        }
     }
   }).mount('#app')
